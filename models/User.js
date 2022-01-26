8a301fe2d36688b2e7bbb7d6b855028a3d4a053f
David Schepis <davidschepis@gmail.com>
2022-01-26 14:46:36 -0700

    fixed code

--- models/User.js
+++ models/User.js
@@ -1,13 +1,5 @@
-
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
-const bcrypt = require("bcrypt");
-
-
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcrypt = require('bcrypt');
-
 
 class User extends Model {
   checkPassword(loginPw) {
@@ -18,86 +10,30 @@ class User extends Model {
 User.init(
   {
     id: {
-
       type: DataTypes.INTEGER,
       allowNull: false,
-    },
-    user_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
       primaryKey: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
       autoIncrement: true,
-
     },
     user_name: {
       type: DataTypes.STRING,
       allowNull: false,
-
     },
-
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-
-      }
-    },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [8],
-        },
-      }
-
-      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
-
+    }
   },
   {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
-        return updatedUserData;
-      },
-
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-
-    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-
-    modelName: "user",
-
     modelName: 'user',
-
   }
 );
 
