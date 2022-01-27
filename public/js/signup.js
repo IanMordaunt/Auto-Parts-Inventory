bd5c38d1edb8a547bc748e29d76aa64a99f11f14
Ian <new@news-MacBook-Pro.local>
2022-01-27 14:21:15 -0700

    update to logout.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,25 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const user_name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+  const user_name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
-  
-    if (user_name && email && password) {
-      const response = await fetch('/signup', {
-        method: 'POST',
-        body: JSON.stringify({ user_name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/parts');
-      } else {
-        alert(response.statusText);
-      }
+  if (user_name && email && password) {
+    const response = await fetch("/signup", {
+      method: "POST",
+      body: JSON.stringify({ user_name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/parts");
+    } else {
+      alert(response.statusText);
     }
-  };
+  }
+};
 
-  document.getElementById("signup-form").addEventListener("submit", signupFormHandler);
+document
+  .getElementById("signup-form")
+  .addEventListener("submit", signupFormHandler);
