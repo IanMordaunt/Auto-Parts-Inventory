8ff8220bce8d41a76c65be18930416f7b3aa35cc
David Schepis <davidschepis@gmail.com>
2022-01-30 14:03:25 -0700

    added more seeds

--- public/js/client.js
+++ public/js/client.js
@@ -2,7 +2,7 @@ async function doSearch() {
 
   var select = document.getElementById('categoryDropdown');
   var value = select.options[select.selectedIndex].value;
-  if (value === "engine" || value === "transmission" || value === "electrical" || value === "suspension" || value === "ac" || value === "exhaust system" || value === "brake system") {
+  if (value === "engine" || value === "transmission" || value === "electrical" || value === "suspension" || value === "ac" || value === "exhaust system" || value === "brake system" || value === "misc") {
     document.location.replace(`/partsPage/${value}`);
   } else {
     alert("nothing was selected in a search bar")
